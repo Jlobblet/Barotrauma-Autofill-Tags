535d0e92f063dfd0d461574b44ee615d9231fdff
Jlobblet <jlobblet+github@jlobblet.co.uk>
2024-05-11 13:03:03 +0100

    Remove unused configuration parameter

--- src/Barotrauma-Autofill-Tags/Settings.fs
+++ src/Barotrauma-Autofill-Tags/Settings.fs
@@ -5,14 +5,12 @@ open Argu
 
 type Arguments =
     | [<Unique; CustomAppSettings("OutputLocation")>] OutputLocation of path: string
-    | [<Unique; CustomAppSettings("TemplateLocation")>] TemplateLocation of path: string
     | [<Unique; CustomAppSettings("BarotraumaLocation")>] BarotraumaLocation of path: string
 
     interface IArgParserTemplate with
         member s.Usage =
             match s with
             | OutputLocation _ -> "The directory to put output files"
-            | TemplateLocation _ -> "Path to the template for the order of tags"
             | BarotraumaLocation _ -> "The location where Barotrauma is installed"
 
 let errorHandler =
@@ -28,7 +26,6 @@ let Parser = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
 [<Struct>]
 type Settings =
     { OutputLocation: string
-      TemplateLocation: string
       BarotraumaLocation: string }
 
     static member FromArgv argv =
@@ -37,12 +34,7 @@ type Settings =
         let outputLocation =
             results.TryGetResult <@ OutputLocation @> |> Option.defaultValue "."
 
-        let templateLocation =
-            results.TryGetResult <@ TemplateLocation @>
-            |> Option.defaultValue "template.txt"
-
         let barotraumaLocation = results.GetResult <@ BarotraumaLocation @>
 
         { OutputLocation = outputLocation
-          TemplateLocation = templateLocation
           BarotraumaLocation = barotraumaLocation }
